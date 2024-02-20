3fcd4ad1cef38b56c4a894e5983f81777ff12f40
root <root@ip-172-31-30-152.ap-southeast-1.compute.internal>
2024-02-20 10:16:31 +0000

    first change from xyz

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('sare jaha se acha hindosta hamara yah bulbule hai iski yah gulsita hamara!'));
+app.get('/', (req, res) => res.send('jai maharashtra!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
