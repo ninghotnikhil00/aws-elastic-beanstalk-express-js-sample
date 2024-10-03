52bb8773dee232cf47523949b9762c7f07df1cbd
root <root@ip-172-31-24-219.ap-south-1.compute.internal>
2024-10-03 05:15:23 +0000

    mycommit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('jai hind jai maharashtra!'));
+app.get('/', (req, res) => res.send('jai hind jai maharashtra jai jagat!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
